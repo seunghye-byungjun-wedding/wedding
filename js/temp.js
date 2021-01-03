98e180af11ac642e16a17a0f78be498c841fff68
seunghye-byungjun-wedding <76855401+seunghye-byungjun-wedding@users.noreply.github.com>
2021-01-03 10:59:48 +0900

    Update temp.js

--- js/temp.js
+++ js/temp.js
@@ -1,56 +1,27 @@
-function modal(id) {
-    var zIndex = 9999;
-    var modal = document.getElementById(id);
+	var modal = document.getElementById("myModal");
 
-    // 모달 div 뒤에 희끄무레한 레이어
-    var bg = document.createElement('div');
-    bg.setStyle({
-        position: 'fixed',
-        zIndex: zIndex,
-        left: '0px',
-        top: '0px',
-        width: '100%',
-        height: '100%',
-        overflow: 'auto',
-        // 레이어 색갈은 여기서 바꾸면 됨
-        backgroundColor: 'rgba(0,0,0,0.4)'
-    });
-    document.body.append(bg);
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
 
-    // 닫기 버튼 처리, 시꺼먼 레이어와 모달 div 지우기
-    modal.querySelector('.modal_close_btn').addEventListener('click', function() {
-        bg.remove();
-        modal.style.display = 'none';
-    });
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("modal-close")[0];
 
-    modal.setStyle({
-        position: 'fixed',
-        display: 'block',
-        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+	// When the user clicks the button, open the modal
+	btn.onclick = function() {
+	  modal.style.display = "block";
+	}
 
-        // 시꺼먼 레이어 보다 한칸 위에 보이기
-        zIndex: zIndex + 1,
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
 
-        // div center 정렬
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        msTransform: 'translate(-50%, -50%)',
-        webkitTransform: 'translate(-50%, -50%)'
-    });
-}
-
-// Element 에 style 한번에 오브젝트로 설정하는 함수 추가
-Element.prototype.setStyle = function(styles) {
-    for (var k in styles) this.style[k] = styles[k];
-    return this;
-};
-
-document.getElementById('popup_open_btn').addEventListener('click', function() {
-    // 모달창 띄우기
-    modal('my_modal');
-});
-
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+	    modal.style.display = "none";
+	  }
+	}
 
 function copyToClipboard(id) {
     //var text = document.getElementById(id).innerText;
